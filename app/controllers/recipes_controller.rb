9c029501e1cd6c924be90852d1e3d5d7fdda9f8d
jwats287 <jwats287@users.noreply.github.com>
2017-08-23 14:16:44 -0400

    Added new and create routes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,6 +7,21 @@ class RecipesController < ApplicationController
     end
 
     def new
+        @recipe = Recipe.new
+    end
+
+    def create
+        @recipe = Recipe.new(recipe_params)
+        if @recipe.save
+            redirect_to recipes_url
+        else
+            redirect_to new_recipe_url
+        end
+    end
+
+    private
+    def recipe_params
+        params.require(:recipe).permit(:title, :description, :made_it_yet)
     end
 
 end
